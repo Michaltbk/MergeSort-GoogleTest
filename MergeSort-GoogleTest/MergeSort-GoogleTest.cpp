d5b16f1d57b9c79a5ddcb476f3490d21de2bea46
Michaltbk <100926282+Michaltbk@users.noreply.github.com>
2024-11-28 21:26:50 +0100

    Dodano zawartość main oraz poprawiono błąd

--- MergeSort-GoogleTest/MergeSort-GoogleTest.cpp
+++ MergeSort-GoogleTest/MergeSort-GoogleTest.cpp
@@ -1,10 +1,25 @@
 ﻿#include <iostream>
 #include "MergeSort.h"
+
 using namespace std;
 
+int main() {
+    int array[] = { 38, 27, 43, 3, 9, 82, 10 };
+    int size = sizeof(array) / sizeof(array[0]);
 
-int main()
-{
-    
+    cout << "Przed sortowaniem: ";
+    for (int i = 0; i < size; ++i) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+
+    MergeSort::sort(array, size);
+
+    cout << "Po sortowaniu: ";
+    for (int i = 0; i < size; ++i) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
-
