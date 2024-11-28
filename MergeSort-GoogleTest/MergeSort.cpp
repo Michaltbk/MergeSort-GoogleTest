fda0a1bbcc8e2e95d26aa4093e34a2114a3c88bc
Michaltbk <100926282+Michaltbk@users.noreply.github.com>
2024-11-28 21:00:22 +0100

    Dodano szkielet klasy MergeSort

--- MergeSort-GoogleTest/MergeSort.cpp
+++ MergeSort-GoogleTest/MergeSort.cpp
@@ -1,3 +1,14 @@
 #include "MergeSort.h"
 using namespace std;
 
+void MergeSort::sort(int* array, int size) {
+    // Implementacja w kolejnych krokach
+}
+
+void MergeSort::mergeSort(int* array, int left, int right) {
+    // Implementacja w kolejnych krokach
+}
+
+void MergeSort::merge(int* array, int left, int mid, int right) {
+    // Implementacja w kolejnych krokach
+}
--- MergeSort-GoogleTest/MergeSort.h
+++ MergeSort-GoogleTest/MergeSort.h
@@ -1,7 +1,12 @@
 #pragma once
 using namespace std;
 
-class MergeSort
-{
+class MergeSort {
+public:
+    static void sort(int* array, int size);
+
+private:
+    static void mergeSort(int* array, int left, int right);
+    static void merge(int* array, int left, int mid, int right);
 };
 
