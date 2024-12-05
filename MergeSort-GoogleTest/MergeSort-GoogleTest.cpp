e66c1bde107fc3ce86f3689af06f6ea44c326cf3
Michaltbk <100926282+Michaltbk@users.noreply.github.com>
2024-12-05 18:39:05 +0100

    Revert "Dodano zawartość main oraz poprawiono błąd"
    This reverts commit d5b16f1d57b9c79a5ddcb476f3490d21de2bea46.

--- MergeSort-GoogleTest/MergeSort-GoogleTest.cpp
+++ MergeSort-GoogleTest/MergeSort-GoogleTest.cpp
@@ -1,25 +1,10 @@
 ﻿#include <iostream>
 #include "MergeSort.h"
-
 using namespace std;
 
-int main() {
-    int array[] = { 38, 27, 43, 3, 9, 82, 10 };
-    int size = sizeof(array) / sizeof(array[0]);
 
-    cout << "Przed sortowaniem: ";
-    for (int i = 0; i < size; ++i) {
-        cout << array[i] << " ";
-    }
-    cout << endl;
-
-    MergeSort::sort(array, size);
-
-    cout << "Po sortowaniu: ";
-    for (int i = 0; i < size; ++i) {
-        cout << array[i] << " ";
-    }
-    cout << endl;
-
-    return 0;
+int main()
+{
+    
 }
+
--- MergeSort-GoogleTest/MergeSort.cpp
+++ MergeSort-GoogleTest/MergeSort.cpp
@@ -1,52 +1,51 @@
 #include "MergeSort.h"
-
 using namespace std;
 
 void MergeSort::sort(int* array, int size) {
-    if (size > 1) {
-        mergeSort(array, 0, size - 1);
-    }
+	if (size > 1) {
+		mergeSort(array, 0, size - 1);
+	}
 }
 
 void MergeSort::mergeSort(int* array, int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
-        mergeSort(array, left, mid);
-        mergeSort(array, mid + 1, right);
-        merge(array, left, mid, right);
-    }
+	if (left < right) {
+		int mid = left + (right - left) / 2;
+		mergeSort(array, left, mid);
+		mergeSort(array, mid + 1, right);
+		//merge(array, left, mid, right); 
+	}
 }
 
 void MergeSort::merge(int* array, int left, int mid, int right) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
+	int n1 = mid - left + 1;
+	int n2 = right - mid;
 
-    int* leftArray = new int[n1];
-    int* rightArray = new int[n2];
+	int* leftArray = new int[n1];
+	int* rightArray = new int[n2];
 
-    for (int i = 0; i < n1; ++i)
-        leftArray[i] = array[left + i];
-    for (int i = 0; i < n2; ++i)
-        rightArray[i] = array[mid + 1 + i];
+	for (int i = 0; i < n1; ++i)
+		leftArray[i] = array[left + i];
+	for (int i = 0; i < n2; ++i)
+		rightArray[i] = array[mid + 1 + i];
 
-    int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2) {
-        if (leftArray[i] <= rightArray[j]) {
-            array[k++] = leftArray[i++];
-        }
-        else {
-            array[k++] = rightArray[j++];
-        }
-    }
+	int i = 0, j = 0, k = left;
+	while (i < n1 && j < n2) {
+		if (leftArray[i] <= rightArray[j]) {
+			array[k++] = leftArray[i++];
+		}
+		else {
+			array[k++] = rightArray[j++];
+		}
+	}
 
-    while (i < n1) {
-        array[k++] = leftArray[i++];
-    }
+	while (i < n1) {
+		array[k++] = leftArray[i++];
+	}
 
-    while (j < n2) {
-        array[k++] = rightArray[j++];
-    }
+	while (j < n2) {
+		array[k++] = rightArray[j++];
+	}
 
-    delete[] leftArray;
-    delete[] rightArray;
+	delete[] leftArray;
+	delete[] rightArray;
 }
