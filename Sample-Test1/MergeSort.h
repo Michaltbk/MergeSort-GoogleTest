d285a934661aec4f00f09a7703110cde09b6f171
Michaltbk <100926282+Michaltbk@users.noreply.github.com>
2024-12-05 19:41:58 +0100

    Add files via upload

---
+++ Sample-Test1/MergeSort.h
@@ -0,0 +1,14 @@
+#pragma once
+using namespace std;
+
+class MergeSort {
+public:
+    static void sort(int* array, int size);
+
+private:
+    static void mergeSort(int* array, int left, int right);
+    static void merge(int* array, int left, int mid, int right);
+};
+
+
+
